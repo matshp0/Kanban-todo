75c0eff9ff31f376a6801b9e114a0a8692531d31
matshp0 <matvii.shpyruk@gmail.com>
2025-10-27 11:15:49 +0200

    feat(server): CORS allow all origins

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
